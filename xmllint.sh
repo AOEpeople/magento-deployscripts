aeb8683c2f2f54462a03727f284583a9903b84e2
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-09-18 20:36:21 +0000

    Introduce cache

--- xmllint.sh
+++ xmllint.sh
@@ -11,15 +11,30 @@ command_exists () {
 
 FILES=`find $1 -type f -name "*.xml"`
 
+TMP_FILE=/tmp/xmllint.tmp
+touch $TMP_FILE;
+
 if command_exists xmllint ; then
     for i in $FILES; do
-        xmllint --noout "$i" || { echo "Unable to parse file '$i'"; exit 1; }
-        echo "No syntax errors detected in $i"
+        md5=($(md5sum $i));
+        if grep -Fxq "$md5" $TMP_FILE; then
+            echo "No syntax errors detected in $i (cached)"
+        else
+            xmllint --noout "$i" || { echo "Unable to parse file '$i'"; exit 1; }
+            echo $md5 >> $TMP_FILE
+            echo "No syntax errors detected in $i"
+        fi
     done
 else
     echo "Could not find xmllint. Using PHP instead..."
     for i in $FILES; do
-        php -r "if (@simplexml_load_file('$i') === false) exit(1);" || { echo "Unable to parse file '$i'"; exit 1; }
-        echo "No syntax errors detected in $i"
+        md5=($(md5sum $i));
+        if grep -Fxq "$md5" $TMP_FILE; then
+            echo "No syntax errors detected in $i (cached)"
+        else
+            php -r "if (@simplexml_load_file('$i') === false) exit(1);" || { echo "Unable to parse file '$i'"; exit 1; }
+            echo $md5 >> $TMP_FILE
+            echo "No syntax errors detected in $i"
+        fi
     done
 fi
