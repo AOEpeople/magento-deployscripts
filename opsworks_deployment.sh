359660f6b0aa582582cf3d837543db0550ef5fde
Clemens Queissner <clemens.queissner@cq-design.de>
2017-04-04 15:40:34 -0700

    Added the option to pass in custom json to the deployment

--- opsworks_deployment.sh
+++ opsworks_deployment.sh
@@ -7,6 +7,7 @@ function usage {
     echo " -a   app id";
     echo " -p   aws cli profile";
     echo " -c   command";
+    echo " -C   custom json (Limted to 80kb, needs to be escaped)"
     echo ""
     echo "Example:"
     echo "BUILD=82; $0 -p magento-build-uploads -s 484ddf88-b89f-42a5-ada9-e0f6d0870e83 -a 13db9a00-4398-43b3-99c7-8671490814c0 -c '{\"Name\":\"deploy\"}'"
@@ -18,6 +19,7 @@ case "${OPTION}" in
         s) AWS_STACKID="${OPTARG}";;
         a) AWS_APPID="${OPTARG}";;
         c) COMMAND="${OPTARG}";;
+        C) JSON="${OPTARG}";;
         p) PROFILE="${OPTARG}";;
         \?) echo; usage 1;;
     esac
@@ -31,7 +33,7 @@ if [ -z "${PROFILE}" ]; then echo "ERROR: Please provide a aws cli profile (-p <
 AWSCLI="aws --profile ${PROFILE} --region us-east-1 opsworks"
 
 echo "Triggering deployment on app ${AWS_APPID}"
-DEPLOYMENT_ID=`${AWSCLI} create-deployment --stack-id "${AWS_STACKID}" --app-id "${AWS_APPID}" --command "${COMMAND}" | jq '.DeploymentId'  | sed 's/\"//g'`
+DEPLOYMENT_ID=`${AWSCLI} create-deployment --stack-id "${AWS_STACKID}" --app-id "${AWS_APPID}" --command "${COMMAND}" --custom-json "${JSON}" | jq '.DeploymentId'  | sed 's/\"//g'`
 
 echo "Deployment Id: ${DEPLOYMENT_ID}"
 echo "https://console.aws.amazon.com/opsworks/home#/stack/${AWS_STACKID}/deployments/${DEPLOYMENT_ID}"
